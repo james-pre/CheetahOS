948e41c0231a19af4347b08a6cf66ab32ea26746
chinons098 <chinonsoakolam@outlook.com>
2022-08-04 14:04:50 -0400

    Added a little styled component.

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'cos-title',
+  templateUrl: './title.component.html',
+  styleUrls: ["./title.component.css"]
+})
+
+export class TitleComponent{
+   
+}
