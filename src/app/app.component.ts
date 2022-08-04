dccbf19650566aee62194f54e7966ac0204c14a4
chinons098 <chinonsoakolam@outlook.com>
2022-08-03 22:00:37 -0400

    Added Angular specific linter and Husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'cos-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 
-
+/**
+ *  This is the main app component
+ */
 export class AppComponent {
   title = 'CheetahOS';
 }
