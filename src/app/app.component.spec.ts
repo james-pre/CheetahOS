fe2ef1bd7b0eda3ccf2afa5fbfb800600d920fe5
chinons098 <chinonsoakolam@outlook.com>
2022-08-04 14:43:42 -0400

    fixed the test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import { TitleComponent } from './title/title.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
+        TitleComponent,
         AppComponent,
       ],
     }).compileComponents();
