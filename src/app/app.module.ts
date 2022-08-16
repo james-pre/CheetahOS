4c7d22b168e6758af68a26f9c96ce015d5794675
chinons098 <chinonsoakolam@outlook.com>
2022-08-15 23:26:38 -0400

    simple window component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './system-apps/title/title.component';
+import { WindowComponent } from './system-apps/window/window.component';
 
 @NgModule({
   declarations: [
     TitleComponent,
-    AppComponent
+    AppComponent,
+    WindowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/system-apps/window/window.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cos-window',
+  templateUrl: './window.component.html',
+  styleUrls: ['./window.component.css']
+})
+export class WindowComponent {
+
+  constructor() {
+    //
+   }
+}
