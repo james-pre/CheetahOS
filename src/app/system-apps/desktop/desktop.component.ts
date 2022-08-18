2cbc5fa69d50b11d83b404ada7d69246a64a6462
chinons098 <chinonsoakolam@outlook.com>
2022-08-18 12:57:52 -0400

    desktop is now animated

--- src/app/system-apps/desktop/desktop.component.ts
+++ src/app/system-apps/desktop/desktop.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare let VANTA: {
+  WAVES: (arg0: {
+    el: string; // element selector string or DOM object reference
+    color: number; waveHeight: number; shininess: number; waveSpeed: number; zoom: number;
+  }) => void;
+};
 
 @Component({
   selector: 'cos-desktop',
   templateUrl: './desktop.component.html',
   styleUrls: ['./desktop.component.css']
 })
-export class DesktopComponent {
+export class DesktopComponent implements AfterViewInit{
 
-  constructor() {
-    //
-   }
+  constructor() {//
+  }
+
+  ngAfterViewInit(): void {
+    VANTA.WAVES({
+      el: '#vanta', // element selector string or DOM object reference
+      color: 0x5588,
+      waveHeight: 20,
+      shininess: 50,
+      waveSpeed: 1.5,
+      zoom: 0.75
+    })
+  }
 
 }
