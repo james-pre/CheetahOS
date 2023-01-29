7c8c27cbe191d7cb821b48cb82e0ba586068553e
chinons098 <chinonsoakolam@outlook.com>
2023-01-28 20:21:47 -0500

    changed any to unkown in SessionManagmentService

--- src/app/shared/system-service/session.management.service.ts
+++ src/app/shared/system-service/session.management.service.ts
@@ -7,7 +7,7 @@ import {Injectable } from "@angular/core";
 export class SessionManagmentService{
 
     private sessionName = "main-session";
-    private _sessionDataDict: Map<string, any>; 
+    private _sessionDataDict: Map<string, unknown>; 
     
     constructor(){
         if(sessionStorage.getItem(this.sessionName)){
@@ -15,17 +15,17 @@ export class SessionManagmentService{
             this._sessionDataDict = new Map(JSON.parse(sessData));
         }
         else{
-            this._sessionDataDict = new  Map<string, any>();
+            this._sessionDataDict = new  Map<string, unknown>();
         }
     }
 
-    addSession(key:string, dataToAdd:any): void{
+    addSession(key:string, dataToAdd:unknown): void{
 
         this._sessionDataDict.set(key,dataToAdd)
         this.saveSession(this._sessionDataDict);
     }
 
-    getSession(key:string):any{
+    getSession(key:string):unknown{
         const stateData = this._sessionDataDict.get(key);
         return stateData;
     }
@@ -36,11 +36,11 @@ export class SessionManagmentService{
     }
 
     resetSession(): void{
-        this._sessionDataDict = new Map<string, any>;
+        this._sessionDataDict = new Map<string, unknown>;
         sessionStorage.clear()
     }
 
-    private saveSession(sessionData:Map<string, any>){
+    private saveSession(sessionData:Map<string, unknown>){
         const data =  JSON.stringify(Array.from(sessionData.entries()));
         sessionStorage.setItem(this.sessionName, data);
     }
