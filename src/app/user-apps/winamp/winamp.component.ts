82d60ea9be5bbb1c146dce13e787a37377de7d1f
chinons098 <chinonsoakolam@outlook.com>
2023-10-23 09:05:14 -0400

    missed this

---
+++ src/app/user-apps/winamp/winamp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cos-winamp',
+  templateUrl: './winamp.component.html',
+  styleUrls: ['./winamp.component.css']
+})
+export class WinampComponent {
+
+}
