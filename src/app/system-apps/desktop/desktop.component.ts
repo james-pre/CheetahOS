31b3a40f73d854640dad6e8d0a4291f5e0b385d8
chinons098 <chinonsoakolam@outlook.com>
2023-10-23 13:10:57 -0400

    did some rework to the desktop and vanta.js

--- src/app/system-apps/desktop/desktop.component.ts
+++ src/app/system-apps/desktop/desktop.component.ts
@@ -1,21 +1,51 @@
-import { AfterViewInit, OnInit, Component } from '@angular/core';
-import { takeWhile, tap, timer } from 'rxjs';
+import { AfterViewInit, OnInit,OnDestroy, Component } from '@angular/core';
+import { Subscription, takeWhile, tap, timer } from 'rxjs';
 import { ProcessIDService } from 'src/app/shared/system-service/process.id.service';
 import { RunningProcessService } from 'src/app/shared/system-service/running.process.service';
 import { ComponentType } from 'src/app/system-files/component.types';
 import { Process } from 'src/app/system-files/process';
-import {WAVES} from './vanta-object/wave';
-declare const VANTA:WAVES 
+import WAVES from 'vanta/dist/vanta.waves.min';
+import BIRDS from 'vanta/dist/vanta.birds.min';
+// import FOG from 'vanta/dist/vanta.fog.min';
+// import CLOUDS from 'vanta/dist/vanta.clouds.min';
+// import CLOUDS2 from 'vanta/dist/vanta.clouds2.min';
+// import GLOBE from 'vanta/dist/vanta.globe.min';
+// import NET from 'vanta/dist/vanta.net.min';
+// import CELLS from 'vanta/dist/vanta.cells.min';
+// import TRUNK from 'vanta/dist/vanta.trunk.min';
+// import TOPOLOGY from 'vanta/dist/vanta.topology.min';
+// import DOTS from 'vanta/dist/vanta.dots.min';
+// import RINGS from 'vanta/dist/vanta.rings.min';
+// import HALO from 'vanta/dist/vanta.halo.min';
+import * as THREE from 'three';
+
+
+const VANTAS = [
+  WAVES,
+  BIRDS,
+  // FOG,
+  // CLOUDS,
+  // CLOUDS2,
+  // GLOBE,
+  // NET,
+  // CELLS,
+  // TRUNK,
+  // TOPOLOGY,
+  // DOTS,
+  // RINGS,
+  // HALO
+];
 
 @Component({
   selector: 'cos-desktop',
   templateUrl: './desktop.component.html',
   styleUrls: ['./desktop.component.css']
 })
-export class DesktopComponent implements OnInit, AfterViewInit{
+export class DesktopComponent implements OnInit, OnDestroy, AfterViewInit{
 
-  private _processIdService;
-  private _runningProcessService;
+  private _processIdService:ProcessIDService;
+  private _runningProcessService:RunningProcessService;
+  private _timerSubscription!: Subscription;
 
   hasWindow = false;
   icon = '';
@@ -23,45 +53,56 @@ export class DesktopComponent implements OnInit, AfterViewInit{
   processId = 0;
   type = ComponentType.systemComponent;
   displayName = '';
-  defaultColor!:WAVES;
+  private vantaEffect: any;
 
   constructor( processIdService:ProcessIDService,runningProcessService:RunningProcessService) { 
     this._processIdService = processIdService;
     this._runningProcessService = runningProcessService;
     this.processId = this._processIdService.getNewProcessId()
     this._runningProcessService.addProcess(this.getComponentDetail());
-    this.defaultColor = new WAVES('#vanta',0x274c, 20, 50, 0.5, 0.75);
+    //this.defaultColor = new WAVES('#vanta',0x274c, 20, 50, 0.5, 0.75);
   }
 
 
   ngOnInit():void{
-    VANTA.WAVES(this.defaultColor)
+   
 
       //I'm unable to acccess vanta js effect.
       // const effect = VANTA.WAVES({
       //   el: '#my-background',
       //   color: 0x000000
       // })
+
+    1
           
+  }
+
+  ngOnDestroy(): void {
+    this._timerSubscription?.unsubscribe();
+    this.vantaEffect?.destroy();
   }
 
 
   ngAfterViewInit():void{
-    1
-    //this.rotateColors();
+    const vanta = VANTAS[0];
+    this.vantaEffect = vanta({
+      el: '#vanta',
+      color:0x274c,   
+      THREE: THREE,    
+    });
   }
 
   private rotateColors():void{
 
     let counter = 0;
     const colorSet = [0x284a,0x294c,0x304d,0x314b,0x324c,0x334c,0x344c,0x354c,0x364c,0x374c,0x384c,0x394c,0x404c,0x414c]
-      timer(1000, 4000) //Initial delay 1 seconds and interval countdown also 4 second
+     this._timerSubscription = timer(1000, 4000) //Initial delay 1 seconds and interval countdown also 4 second
         .pipe( takeWhile(() => counter < 14 ), tap(() => counter++))
         .subscribe(() => {
           if (counter == 14){
                counter = 0
           }
-          this.defaultColor.color = colorSet[counter];
+          //this.defaultColor.color = colorSet[counter];
           //VANTA.WAVES(this.defaultColor)
           //VANTA.color = colorSet[counter];
 
--- src/app/system-apps/desktop/vanta-object/wave.ts
+++ src/app/system-apps/desktop/vanta-object/wave.ts
@@ -6,14 +6,19 @@ export class WAVES {
     shininess!: number;
     waveSpeed!: number;
     zoom!: number;
+    mouseControls: boolean | undefined = true;
+    touchControls: boolean | undefined= true;
+    
 
-    constructor(el:string, color:number, waveHeight:number, shininess:number, waveSpeed: number, zoom: number ) { 
+    constructor(el:string, color:number, waveHeight:number, shininess:number, waveSpeed: number, zoom: number, mouseControls?:boolean, touchControls?:boolean) { 
         this.el = el 
         this.color = color
         this.waveHeight = waveHeight
         this.shininess = shininess
         this.waveSpeed = waveSpeed
         this.zoom = zoom
+        this.mouseControls = mouseControls
+        this.touchControls = touchControls
     }  
 
     WAVES( arg0:WAVES) : void{
@@ -25,6 +30,24 @@ export class WAVES {
         this.waveSpeed = arg0.waveSpeed
         this.zoom = arg0.zoom
     }
+
+     destroy():void {
+        throw new Error("Function not implemented.");
+    }
+
 }
 
+
+
+
+// export interface WAVES {
+//     el?: string;
+//     color: number;
+//     waveHeight: number;
+//     shininess: number;
+//     waveSpeed: number;
+//     zoom: number;
+//     mouseControls: boolean;
+//     touchControls: boolean;
+// }
 
